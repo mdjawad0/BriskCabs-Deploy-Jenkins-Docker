d21301d093c1f123c58629f55c6f046088445ae8
Rishabh Singh <rishimark98@gmail.com>
2022-09-18 11:05:39 +0530

    field validations & footer bg color

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,20 +5,31 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   loginData: any = {};
-  constructor(private _auth: AuthService, private router: Router) { }
+  isUsernameInvalid = false;
+  passwordInvalid = false;
+  constructor(private _auth: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   loginUser() {
+    if (this.loginData.username.trim() === '') {
+      this.isUsernameInvalid = true;
+      return;
+    }
+    if (this.loginData.password.length < 6) {
+      this.passwordInvalid = true;
+      return;
+    }
     this._auth.loginUser(this.loginData).subscribe(
       (res) => {
-        localStorage.setItem('isAdmin', res.isAdmin)
-        res.isAdmin === 'true' ? this.router.navigate(['/admin']) : this.router.navigate(['/booking']);
+        localStorage.setItem('isAdmin', res.isAdmin);
+        res.isAdmin === 'true'
+          ? this.router.navigate(['/admin'])
+          : this.router.navigate(['/booking']);
       },
       (err) => console.log(err)
     );
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,11 +8,46 @@ import { AuthService } from '../auth.service';
 })
 export class RegisterComponent implements OnInit {
   registerUserData: any = {};
+  validateName = false;
+  validateUsername = false;
+  validatePassword = false;
+  validateRc = false;
+  validateDl = false;
+  validateVehicle = false;
+
   constructor(private _auth: AuthService) {}
 
   ngOnInit(): void {}
 
   registerUser() {
+    console.log(this.registerUserData)
+    if (this.registerUserData.name.trim() === '') {
+      this.validateName = true;
+      return;
+    } else {
+      this.validateName = false;
+    }
+    if (this.registerUserData.username.trim() === '') {
+      this.validateUsername = true;
+      return;
+    }
+    if (this.registerUserData.password.trim().length < 6) {
+      this.validatePassword = true;
+      return;
+    }
+    if (this.registerUserData.cabRider && this.registerUserData.registration_certificate.trim() === '') {
+      this.validateRc = true;
+      return;
+    }
+    if (this.registerUserData.cabRider && this.registerUserData.vehicle.trim() === '') {
+      this.validateVehicle = true;
+      return;
+    }
+    if (this.registerUserData.cabRider && this.registerUserData.DL.trim() === '') {
+      this.validateDl = true;
+      return;
+    }
+
     this._auth.registerUser(this.registerUserData).subscribe(
       (res) => console.log(res),
       (err) => console.log(err)
